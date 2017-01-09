d850cb03d65edd895368b68dfaa1812df6bc40c2
ravi kumar <ravi kumar>
2017-01-09 18:48:56 +0530

    boilerplate

--- app/app.jsx
+++ app/app.jsx
@@ -1,9 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
-var Main = require('Main');
-var Countdown = require('Countdown');
-var Timer = require('Timer');
+
 //Load foundation
 require('style!css!foundation-sites/dist/foundation.min.css')
 $(document).foundation();
@@ -12,11 +10,6 @@ $(document).foundation();
 require('style!css!sass!applicationStyles')
 
 ReactDOM.render(
-  <Router history={hashHistory}>
-    <Route path="/" component={Main}>
-      <Route path="countdown" component={Countdown}/>
-      <IndexRoute component={Timer}/>
-    </Route>
-  </Router>, 
+  <div><h1>React bolier plate 3</h1></div>,
   document.getElementById('app')
 );
